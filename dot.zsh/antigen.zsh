8e745b13eb71734b94ab16464aba6ff44cc35095
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-04-07 11:02:38 +0900

    add test-kitchen completion.

--- dot.zsh/antigen.zsh
+++ dot.zsh/antigen.zsh
@@ -1,9 +1,13 @@
 source ~/.zsh/antigen/antigen.zsh
 
 # Bundles from the detault repo
-antigen-bundle git
-antigen-bundle autojump
-antigen-bundle heroku
+antigen bundle git
+antigen bundle autojump
+antigen bundle heroku
+antigen bundle bundler
+antigen bundle git-flow
+antigen bundle rbenv
+antigen bundle zsh-users/zsh-completions src
 
 # Syntax highlighting bundle.
 antigen-bundle zsh-users/zsh-syntax-highlighting
