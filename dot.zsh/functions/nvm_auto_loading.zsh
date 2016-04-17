f39e478d55adc2ce0eb3a58532b6bf1d4f7c8412
hirakiuc <hirakiuc@gmail.com>
2016-04-17 23:31:00 +0900

    add auto-loading .nvmrc

---
+++ dot.zsh/functions/nvm_auto_loading.zsh
@@ -0,0 +1,8 @@
+# https://github.com/creationix/nvm#calling-nvm-use-automatically-in-a-directory-with-a-nvmrc-file
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
