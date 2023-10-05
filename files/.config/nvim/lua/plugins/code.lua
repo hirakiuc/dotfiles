a327e032688ded1a8fbeee69467dfb718024b139
hirakiuc <hirakiuc@gmail.com>
2023-10-05 12:40:25 +0900

    Add current nvim configs

---
+++ files/.config/nvim/lua/plugins/code.lua
@@ -0,0 +1,68 @@
+-- plugins for code
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {}
+  },
+  {
+    'RRethy/nvim-treesitter-endwise',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        endwise = {
+          enable = true,
+        },
+      })
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    event = { "BufRead", "BufNewFile"},
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        init = function()
+          require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+          -- load_textobjects = true
+        end,
+      },
+    },
+    cmd = { "TSUpdateSync" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "bash",
+          "html",
+          "javascript",
+          "jsdoc",
+          "json",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "dockerfile",
+          "diff",
+          "gitignore",
+          "svelte",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "yaml",
+          "go",
+        },
+      })
+    end
+  },
+
+}
