8e5ba248104307a3a339b10af75cfa151b751ff8
hirakiuc <hirakiuc@gmail.com>
2024-10-05 13:43:10 +0900

    Add gcloud-peco function

---
+++ files/.zsh/functions/gcloud-peco.zsh
@@ -0,0 +1,11 @@
+gcloud-peco() {
+  local selected=$(
+    gcloud config configurations list --format='table[no-heading] (is_active.yesno(yes="[x]",no="[_]"), name, properties.core.account, properties.core.project.yesno(no="(unset)"))' \
+      | peco --select-1 --query="$1" \
+      | awk '{print $2}'
+  )
+
+  if [ -n "$selected" ]; then
+    gcloud config configurations activate $selected
+  fi
+}
